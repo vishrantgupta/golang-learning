becb3fb97d2952b29bfe777fc460f74dbdab8805
vishrant.gupta <vishrant.gupta@gmail.com>
2018-04-29 15:22:21 -0500

    Started learning

---
+++ README.md
@@ -0,0 +1,1 @@
+# GoLang learning 
---
+++ helloworld.go
@@ -0,0 +1,18 @@
+// LEARNING:
+
+// execution:
+// go run <filename.go>
+// go build <filename.go>
+// 
+// build will generate binary
+
+// go search for main package
+package main
+
+import "fmt"
+
+// the execution starts from main method
+func main() {
+
+	fmt.Println("Hello GoLang!");
+}
